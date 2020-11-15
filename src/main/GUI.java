f6b9cddf30a015ab4c86b4139750054985467649
vanessachow <vanessa.chow@hotmail.com>
2020-11-14 22:53:27 -0800

    added buttons

--- src/main/GUI.java
+++ src/main/GUI.java
@@ -4,35 +4,44 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-// code modeled after https://introcs.cs.princeton.edu/java/15inout/GUI.java.html
-public class GUI implements ActionListener {
+// code modeled after: https://introcs.cs.princeton.edu/java/15inout/GUI.java.html
+//                     https://beginnersbook.com/2015/07/java-swing-tutorial/
+public class GUI extends JFrame implements ActionListener {
 
     private int count = 0;
-    private JLabel label;
+    private JLabel welcomeLabel;
     private JFrame frame;
     private JPanel panel;
-    private JButton button;
+    private JButton addPersonButton;
+    private JButton viewNamesButton;
+    private JButton deleteRecentButton;
 
     public GUI() {
 
+        this.setResizable(true);
+
         frame = new JFrame();
 
-        button = new JButton("Click me");
-        button.addActionListener(this);
+        JLabel welcomeLabel = new JLabel("Welcome! Please select one of the options below...");
 
-        label = new JLabel("Number of clicks: 0");
-
-
+        addPersonButton = new JButton("Add a person");
+        addPersonButton.addActionListener(this);
+        viewNamesButton = new JButton("View the list of names of visitors");
+        viewNamesButton.addActionListener(this);
+        deleteRecentButton = new JButton("Delete the most recently added visitor");
+        deleteRecentButton.addActionListener(this);
 
         panel = new JPanel();
+        panel.add(welcomeLabel);
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0, 1));
-        panel.add(button);
-        panel.add(label);
+        panel.setLayout(new GridLayout(10, 1));
+        panel.add(addPersonButton);
+        panel.add(viewNamesButton);
+        panel.add(deleteRecentButton);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Our GUI");
+        frame.setTitle("Covid Data Assistant");
         frame.pack();
         frame.setVisible(true);
     }
@@ -43,7 +52,6 @@ public class GUI implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        count++;
-        label.setText("Number of clicks: " + count);
+        System.out.println("Button clicked");
     }
 }
